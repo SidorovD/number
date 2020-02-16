800cc4ee72eeb0c38cf100b94cde9bc9335ff859
Denis Sidorov <denis2534@gmail.com>
2020-02-16 21:49:52 +0400

    improve docs

--- counter.go
+++ counter.go
@@ -1,15 +1,16 @@
 package counter
 
-const MaxUint = ^uint(0) // set max value by reversing all bits
+const MaxUint = ^uint(0) // set max uint value by reversing all bits
 
+// Counter is imutable one-directional overflowable counter. After it reaches its upper bound (maxVal), counter will reset its value
 type Counter struct {
 	counter uint
 	maxVal  uint
 }
 
-// New creates a new counter started with zero and max value equals to MaxUint
+// New creates a new counter that started from zero and max value that equals to MaxUint
 func New() Counter {
-	return Counter {
+	return Counter{
 		maxVal: MaxUint,
 	}
 }
@@ -25,8 +26,8 @@ func (c Counter) Val() uint {
 }
 
 // Inc increments the counter and returns a new Counter.
-// Inc increments the value until it becomes the max value, after the next increment,
-// the value will be set to zero
+// The counter is incremented until it reaches the max value.
+// After this, the next Inc will "overflow" the counter i.e. reset its value
 func (c Counter) Inc() Counter {
 	if c.counter < c.maxVal {
 		c.counter++
@@ -37,12 +38,11 @@ func (c Counter) Inc() Counter {
 }
 
 // SetMaxValue sets a new max value and returns a new Counter.
-// If the new max value is less than current value, then counter drops its value
+// If the new max value is less than current value, then counter resets
 func (c Counter) SetMaxValue(n uint) Counter {
 	if n < c.counter {
 		c.counter = 0
 	}
-
 	c.maxVal = n
 	return c
 }
